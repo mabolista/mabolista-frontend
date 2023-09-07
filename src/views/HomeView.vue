55107e6df614f49bd974486553acc920ec357b07
iMin <syahjudinazra@gmail.com>
2023-09-07 17:14:13 +0700

    update 7 september 2023

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -43,10 +43,10 @@ const features = [
           </div>
 
           <div class="hidden md:flex items-center gap-3 font-mabolistafont text-2xl">
-            <RouterLink to="/">Home</RouterLink>
-            <RouterLink to="/about">About</RouterLink>
-            <RouterLink to="/events">Events</RouterLink>
-            <RouterLink to="/contact">Contact</RouterLink>
+            <RouterLink class="hover:text-yellow-300" to="/">Home</RouterLink>
+            <RouterLink class="hover:text-yellow-300" to="/about">About</RouterLink>
+            <RouterLink class="hover:text-yellow-300" to="/events">Events</RouterLink>
+            <RouterLink class="hover:text-yellow-300" to="/contact">Contact</RouterLink>
           </div>
         </div>
 
@@ -132,11 +132,9 @@ const features = [
           iure ea. Repellendus totam ipsa nisi laboriosam!
         </p>
         <div class="py-5">
-          <a
-            href="#"
-            class="text-white bg-yellow-500 hover:bg-yellow-700 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-yellow-600 dark:hover:bg-yellow-700 focus:outline-none dark:focus:ring-yellow-800"
-            >Join Now</a
-          >
+          <button data-modal-target="authentication-modal" data-modal-toggle="authentication-modal" class="block text-white bg-yellow-400 hover:bg-yellow-300 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">
+          Join Now
+        </button>
         </div>
       </div>
       <div class="lg:w-1/3">
@@ -146,13 +144,13 @@ const features = [
   </div>
 
   <!-- ---------- SPONSOR ----------  -->
-  <div class="bg-white py-24 sm:py-32">
+  <div class="bg-white py-24 sm:py-32 flex items-center justify-center">
     <div class="mx-auto max-w-7xl px-6 lg:px-8">
       <h2 class="text-center text-3xl font-mabolistafont leading-8 text-gray-900">
         Our Sponsorship
       </h2>
       <div
-        class="mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-5"
+        class="mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-4"
       >
         <img
           class="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
@@ -162,7 +160,7 @@ const features = [
           height="48"
         />
         <img
-          class="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+          class="col-span-2 object-contain h-20 w-32 lg:col-span-1 justify-self-center"
           src="/src/assets/img/logokratingdaeng.png"
           alt="Reform"
           width="158"
@@ -176,7 +174,7 @@ const features = [
           height="48"
         />
         <img
-          class="col-span-2 max-h-12 w-full object-contain sm:col-start-2 lg:col-span-1"
+          class="col-span-2 object-contain h-24 w-32 mt-4 sm:col-start-2 lg:col-span-1 justify-self-center"
           src="/src/assets/img/logopanguripan.png"
           alt="SavvyCal"
           width="158"
@@ -185,6 +183,39 @@ const features = [
       </div>
     </div>
   </div>
+
+  <!-- ---------- JOIN LIST MODAL ----------  -->
+  
+<!-- Main modal -->
+<div id="authentication-modal" tabindex="-1" aria-hidden="true" class="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
+    <div class="relative w-full max-w-md max-h-full">
+        <!-- Modal content -->
+        <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+            <button type="button" class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="authentication-modal">
+                <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                </svg>
+                <span class="sr-only">Close modal</span>
+            </button>
+            <div class="px-6 py-6 lg:px-8">
+                <h3 class="mb-4 text-xl font-medium text-gray-900 dark:text-white">Isi List Dulu ya</h3>
+                <form class="space-y-6" action="#">
+                    <div>
+                        <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nama</label>
+                        <input type="text" name="name" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="Nama" required>
+                    </div>
+                    <div>
+                        <label for="position" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Posisi</label>
+                        <input type="text" name="position" id="position" placeholder="Pemain / Kiper" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required>
+                    </div>
+                    <button type="submit" class="w-full text-white bg-yellow-400 hover:bg-yellow-300 focus:ring-4 focus:outline-none focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-yellow-600 dark:hover:bg-yellow-700 dark:focus:ring-yellow-800">Submit</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</div> 
+
+  <!-- ---------- END JOIN LIST MODAL ----------  -->
 
   <!-- ---------- EVENTS ----------  -->
   <div class="container flex justify-center items-center mx-auto">
@@ -261,8 +292,10 @@ const features = [
   <div class="bg-white py-24 sm:py-32">
     <div class="mx-auto max-w-7xl px-6 lg:px-8">
       <div class="mx-auto max-w-2xl lg:text-center">
-        <h2 class="text-base font-semibold leading-7 text-yellow-500">Benefits</h2>
-        <p class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+        <h2 class="text-center text-3xl font-mabolistafont leading-8">
+        Benefits
+      </h2>
+        <p class="mt-2 text-3xl font-bold tracking-tight text-yellow-500 sm:text-4xl">
           Everything you need to refresh your feelings
         </p>
         <p class="mt-6 text-lg leading-8 text-gray-600">
@@ -292,109 +325,38 @@ const features = [
 
   <!-- ---------- Upcoming Events ----------  -->
 
-  <div class="container mx-auto px-4 mb-5">
-    <div class="flex items-center justify-between">
-      <h2 class="font-semibold mb-4 py-4 md:font-bold md:text-3xl text-xl">Upcoming Events</h2>
-      <div class="block">
-        <a href="" class="">Explore more events</a>
+  <div class="card bg-gray-800 card sm:flex md:flex">
+  <div class="sm:flex-shrink-0 md:flex-shrink-0">
+    <img class="imagine w-full sm:w-56 md:w-56 lg:w-56" src="/src/assets/img/NYTC sawangan.jpg" alt="NYTC Sawangan">
+  </div>
+  <div class="righty ml-3 md:mt-0 md:ml-6 sm:ml-3">
+    <div class="grid grid-cols-2 gap-4">
+  <div> 
+    <div class="text-gray-100 mt-2 mb-4 uppercase tracking-wide text-lg text-600 font-bold cardheading cardtitle">INTERNAL FUN MATCH</div>
+    <div class="text-gray-100 mb-4 uppercase tracking-wide text-lg text-600 font-bold cardheading"><i class="material-icons">
+schedule
+</i><small class="text-gray-200 mb-1 uppercase tracking-wide text-400 cardheading"> 16.00pm - 18.00pm</small></div>
+    
       </div>
-    </div>
+  <!-- ... -->
+   <div class="lefting">
+   <div class="date-ribbon border-2 border-gray-200 mr-4">
+            <h2 class="text-gray-800 tracking-wide text-lg text-600 font-bold">SEP</h2>
+            <h1 class="text-gray-800 tracking-wide text-xl text-600 font-bold">30</h1>
 
-    <div class="grid grid-cols-4 grid-rows-1 gap-3">
-      <div>
-        <div class="max-w-sm rounded overflow-hidden shadow-lg">
-          <img
-            class="w-full"
-            src="/src/assets/img/kingkong soccer arena.jpg"
-            alt="Sunset in the mountains"
-          />
-          <div class="px-6 py-4">
-            <div class="font-bold text-xl mb-2">King Kong Arena</div>
-            <p class="text-gray-700 text-base">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla!
-              Maiores et perferendis eaque, exercitationem praesentium nihil.
-            </p>
-          </div>
-          <div class="px-6 py-6">
-            <a
-              href="#"
-              class="text-white bg-yellow-500 hover:bg-yellow-700 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-yellow-600 dark:hover:bg-yellow-700 focus:outline-none dark:focus:ring-yellow-800"
-              >Join List</a
-            >
-          </div>
-        </div>
-      </div>
-      <div>
-        <div class="max-w-sm rounded overflow-hidden shadow-lg">
-          <img
-            class="w-full"
-            src="/src/assets/img/kingkong soccer arena.jpg"
-            alt="Sunset in the mountains"
-          />
-          <div class="px-6 py-4">
-            <div class="font-bold text-xl mb-2">King Kong Arena</div>
-            <p class="text-gray-700 text-base">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla!
-              Maiores et perferendis eaque, exercitationem praesentium nihil.
-            </p>
-          </div>
-          <div class="px-6 py-6">
-            <a
-              href="#"
-              class="text-white bg-yellow-500 hover:bg-yellow-700 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-yellow-600 dark:hover:bg-yellow-700 focus:outline-none dark:focus:ring-yellow-800"
-              >Join List</a
-            >
-          </div>
-        </div>
-      </div>
-      <div>
-        <div class="max-w-sm rounded overflow-hidden shadow-lg">
-          <img
-            class="w-full"
-            src="/src/assets/img/kingkong soccer arena.jpg"
-            alt="Sunset in the mountains"
-          />
-          <div class="px-6 py-4">
-            <div class="font-bold text-xl mb-2">King Kong Arena</div>
-            <p class="text-gray-700 text-base">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla!
-              Maiores et perferendis eaque, exercitationem praesentium nihil.
-            </p>
-          </div>
-          <div class="px-6 py-6">
-            <a
-              href="#"
-              class="text-white bg-yellow-500 hover:bg-yellow-700 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-yellow-600 dark:hover:bg-yellow-700 focus:outline-none dark:focus:ring-yellow-800"
-              >Join List</a
-            >
-          </div>
-        </div>
-      </div>
-      <div>
-        <div class="max-w-sm rounded overflow-hidden shadow-lg">
-          <img
-            class="w-full"
-            src="/src/assets/img/kingkong soccer arena.jpg"
-            alt="Sunset in the mountains"
-          />
-          <div class="px-6 py-4">
-            <div class="font-bold text-xl mb-2">King Kong Arena</div>
-            <p class="text-gray-700 text-base">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla!
-              Maiores et perferendis eaque, exercitationem praesentium nihil.
-            </p>
-          </div>
-          <div class="px-6 py-6">
-            <a
-              href="#"
-              class="text-white bg-yellow-500 hover:bg-yellow-700 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-yellow-600 dark:hover:bg-yellow-700 focus:outline-none dark:focus:ring-yellow-800"
-              >Join List</a
-            >
-          </div>
-        </div>
-      </div>
+    </div>
+     </div>
+</div>
+    
+    <div class="block mt-1 mr-6 text-lg md:w-3/4 lg:w-3/4 leading-tight font-semibold text-gray-200 hover:underline">Description</div>
+    <p class="mb-2 mt-2 mr-6 text-gray-300 md:w-3/4 lg:w-3/4 ">Here is a description for the even and a bunch of information about that event.</p>
+    <div class="lefting h-16 mb-2"> 
+          <a class="tickets bg-transparent uppercase  hover:bg-yellow-500 text-gray-100 font-semibold hover:text-black py-2 px-4 border-2 border-yellow-500 hover:border-transparent mb-2 mr-4">
+      Waiting List
+    </a>
     </div>
   </div>
+</div>
 
   <!-- ---------- Footer ----------  -->
 
