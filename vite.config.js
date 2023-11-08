90785316773577c854fcc67e4537427cb629b9dd
iMin <syahjudinazra@gmail.com>
2023-11-08 14:19:19 +0700

    slash

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
   plugins: [vue(), vueJsx()],
   build: {
     rollupOptions: {
-      external: ['./src/assets/img']
+      external: ['lodash']
     }
   },
   resolve: {
