d111e83e91ca87a1b45914a4a23584f15c55f19e
iMin <syahjudinazra@gmail.com>
2023-11-08 15:15:40 +0700

    slash

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,16 @@ export default defineConfig({
   plugins: [vue(), vueJsx()],
   build: {
     rollupOptions: {
-      external: ['vue', 'axios', 'sweetalert2']
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ['vue'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          vue: 'Vue'
+        }
+      }
     }
   },
   resolve: {
