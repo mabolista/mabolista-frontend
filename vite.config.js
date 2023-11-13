1e51085793ec44c0cceff93517685daa932c5786
iMin <syahjudinazra@gmail.com>
2023-11-13 10:25:20 +0700

    slash

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,11 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), vueJsx()],
+  build: {
+    rollupOptions: {
+      external: [/^node:.*/]
+    }
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
